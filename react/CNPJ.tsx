86ca374176c8643ca93fd0958414738e7a22c144
Dassa <dassaevtavares@gmail.com>
2022-05-16 14:27:29 -0300

    Repor
    Weeeeee

---
+++ react/CNPJ.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// const HelloWorld: StoreFrontFunctionComponent = () =>{
+
+const CNPJ = () =>{
+  return (
+    <div>
+
+        <div className='flex-layout-row'>
+          <p>COMPRE COM CNPJ.</p>
+          <button>X</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default CNPJ;
